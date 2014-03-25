8e77d426f35f203de2848d7cccd21fbddcae1733
tsalmon <th_s@hotmail.fr>
2014-03-25 22:50:22 +0100

    save (25 days ????)

--- src/application/views/connexion.php
+++ src/application/views/connexion.php
@@ -1,4 +1,5 @@
 ﻿<div id="middle">
+	
 	<form action="<?php echo URL; ?>Welcome/Connexion" method="POST" id="middleform">
 		<h1 id="hfgl">Bienvenue sur HFGL</h1>
 		<p id="quote">Have Fun, Good Learning</p>
