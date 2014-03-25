c0e85e9bb838e2b939549ceaf472db321e37964f
tsalmon <th_s@hotmail.fr>
2014-03-25 15:48:10 +0100

    Inscription done

---
+++ src/application/controller/Teacher.php
@@ -0,0 +1,8 @@
+<?php
+
+class Teacher extends Controller
+{
+    public function index()
+    {
+    }
+}
--- src/application/views/_templates/header.php
+++ src/application/views/_templates/header.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html  lang="fr" dir="ltr">
+<html lang="en">
 <head>
-    <title>PHP MVC skeleton</title>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords"    content="HFGL, learning, learn, genie_logiciel" />
-    <meta name="author"      content="HAMDANE Yasmine, GUEMOURI Aiman, SALMON Thomas, NGUYEN Thi Quynh Nga, CHEVALIER Josian, VLADISLAV Fitc" />
-    <meta name="description" content="HFGL is a learning webiste" />
+    <title>PHP MVC skeleton</title>
+    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
     <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
-    <link rel="home" title="Home"     href="http://tsalmon.fr/gl" />
+    <?php
+        if($page == "connexion" || $page == "inscription"){ // if Welcome mode add him css  
+            ?><link href="<?php echo URL; ?>public/css/welcome.css" rel="stylesheet"><?php
+        }
+    ?>
+    <link href="<?php echo URL; ?>public/css/<?php echo $page ?>.css" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>public/js/application.js"></script>
+</head>
+<body>
