8bd25d11337443dea203163e9736355e344658e9
tsalmon <th_s@hotmail.fr>
2014-03-25 15:56:11 +0100

    Recreate old views

---
+++ src/application/views/navigation.php
@@ -0,0 +1,5 @@
+<ul id="toolbar">
+	<li><h3>Consulter ses notes</h3></li>
+	<li><h1><a href="index.php?page=suggestions">Voir suggestions cours</a></h1></li>
+	<li><h3>Parametrès du compte</h3></li>
+</ul>
