270820ba836feb511c52582469ce67a40b9145bd
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2014-03-26 22:39:31 +0100

    navigation est modifier

--- src/application/views/navigation.php
+++ src/application/views/navigation.php
@@ -1,8 +1,12 @@
 //navigation pour etudiant
-<ul>
-		<li><a href="#">Mes cours</a></li>
-		<li><a href="mes_notes.php">Mes notes</a></li>
-		<li><a href="index.php?page=suggestions">S'incrire à un cours</a></li>
-		<li><a href="#">Parametrès du compte</a></li>
-		<li><a href="#">Déconnexion</a></li>
-</ul>
+<div id ="menu_wraper">
+	<div id = "menu">
+		<ul>
+			<li><a href="#" class="current">Mes cours</a></li>
+			<li><a href="mes_notes.php" target = "_parent">Mes notes</a></li>
+			<li><a href="index.php?page=suggestions" target = "_parent">S'incrire à un cours</a></li>
+			<li><a href="#" target = "_parent">Parametrès du compte</a></li>
+			<li><a href="#" target = "_parent">Déconnexion</a></li>
+		</ul>
+	</div>
+</div>
