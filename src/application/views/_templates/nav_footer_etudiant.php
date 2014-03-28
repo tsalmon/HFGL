b49ebf7629e72e030a3c10789073507275d9bbc3
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-03-28 16:12:08 +0100

    etudiant voire notes et voire notes de cours html

---
+++ src/application/views/_templates/nav_footer_etudiant.php
@@ -0,0 +1,12 @@
+<div id="scroll">
+      <a title="Scroll to the top" class="top" href="#"><img src="images/top.png" alt="top" /></a>
+</div>
+<footer>
+      <p>
+        <a href="Student">Mes cours</a> | 
+        <a href="Student/Notes">Mes notes</a> |
+        <a href="index.php?page=suggestions">S'incrire à un cours</a> | 
+        <a href="Student/Parametres">Parametrès du compte</a>| <a href="#">Déconnexion</a>
+      </p>
+      <p>Copyright &copy; HFGL| <a href="#">design from M1 Info Paris 7</a></p>
+</footer>
--- src/application/views/student_view_notes.php
+++ src/application/views/student_view_notes.php
@@ -1,21 +1,26 @@
-<div id="wrapper">
-	<?php include("navigation.php"); ?>
-	<div id="center">		
-		<table>
-			<tr>
-				<th>Nom du cours</th>
-				<th>Exercices</th>
-				<th>Projet</th>
-				<th>Examen</th>
-				<th>Finale</th>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-				<td>4</td>
-				<td>5</td>
-			</tr>
-		</table>
-	</div>
+<div id="main">
+
+    <?php include("nav_etudiant.php"); ?>
+
+    <div id="site_content">
+      
+      <?php include("student_sidebar_left.php"); ?>
+
+      <div class="content">
+        
+        <h2>Les notes de tous les cours suivis</h2>
+        <table style="width:100%; border-spacing:0;">
+          <tr><th>Nom du cours</th> <th>Exercices</th><th>Projet</th><th>Examen</th><th>Note finale</th></tr>
+          <tr><td><a href="#">Cours 1</a></td> <td></td><td></td><td></td><td></td></tr>
+          <tr><td><a href="#">Cours 2</a></td> <td></td><td></td><td></td><td></td></tr>
+          <tr><td><a href="#">Cours 3</a></td> <td></td><td></td><td></td><td></td></tr>
+          <tr><td><a href="#">Cours 4</a></td> <td></td><td></td><td></td><td></td></tr>
+        </table>
+          
+      </div>
+    </div>
+    
+    <?php include("_templates/nav_footer_etudiant.php"); ?>
 </div>
+
+
