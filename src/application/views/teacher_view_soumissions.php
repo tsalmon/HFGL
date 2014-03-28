7b16132dd87d56ffca6058784620f7d12054c144
GUEMOURI AIMAN <aymanove.48@gmail.com>
2014-03-28 14:28:42 +0100

    enseignant, enseignant_views_soumission

---
+++ src/application/views/teacher_view_soumissions.php
@@ -0,0 +1,21 @@
+<div id="wrapper">
+	<?php include("navigation.php"); ?>
+	<div id="center">		
+		<table>
+			<tr>
+				<th>Nom du cours</th>
+				<th>Exercices</th>
+				<th>Projet</th>
+				<th>Examen</th>
+				<th>Finale</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+			</tr>
+		</table>
+	</div>
+</div>
