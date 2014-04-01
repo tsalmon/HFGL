a65d260a6ffc2589d3ac87e5bca89c188cf8f59b
Vladislav Fitz <vladislav.fitz@gmail.com>
2014-04-01 15:34:13 +0200

    merge

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
  /* Configuration for: Project URL*/
-define('URL', 'http://localhost:8888/GL/src/');
+define('URL', 'http://localhost:8888/src/');
 //define('URL', 'http://localhost:8888/GL/src/');
 //define('URL', 'http://tsalmon.fr/GL/src/');
 
