12b3df36a3764a2d77dc716707c88459cd627556
Vladislav Fitz <vladislav.fitz@gmail.com>
2014-04-20 17:25:26 +0400

    garbage enlevé

--- src/application/controller/Exercicecontroller.php
+++ src/application/controller/Exercicecontroller.php
@@ -25,30 +25,4 @@ class Exercicecontroller extends Controller{
 		require 'application/views/_templates/header.php';
         require 'application/views/_templates/footer.php';
 	}
-
-    /* demenagé à view exercice.php
-    public function showExerciseSheet(){
-        foreach($this->exerciseSheetModel->getQuestions() as $question) {
-            echo $question->getAssignment().'<br/>';
-            //secho $question->getAnswers();
-            $curanswers = $question->getAnswers();
-            echo '<form action="">';
-            foreach($curanswers as $answer) {
-                if($question instanceof QCMQuestion)
-                {
-                    echo '<input type="checkbox" name="checkboxanswer" value="val">'.$answer->getContent().'<br>';
-                }
-                else if($question instanceof QRFQuestion || $question instanceof LQuestion || $question instanceof PQuestion)
-                {
-                    echo '<input type="text" name="textanswer" placeholder="Your answer...">';
-                }
-                else
-                {
-                    throw new Exception("Undefined question type");
-                }
-            }
-            echo '</form>';
-            echo '<br/>';
-        }
-    }*/
 }
