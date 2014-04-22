4d6bba49c0f845415e2ea78847e0266c0ea663c8
tsalmon <th_s@hotmail.fr>
2014-04-22 22:40:28 +0200

    merge coursecontroller

--- src/application/controller/Coursescontroller.php
+++ src/application/controller/Coursescontroller.php
@@ -1,19 +1,7 @@
 <?php
 class Coursescontroller extends Controller{
 	public function index(){
-		$MODELcours = $this->loadModel('CourseSubstcription');
 		
-		$student=&PersonFactory::getPerson($_SESSION["email"]);
-		$cours = $MODELcours->getCourses($student);	
-
-		//print_r($_GET);
-		require 'application/views/_templates/header.php';
-        require 'application/views/student_view_notesDeCours.php';
-        require 'application/views/_templates/footer.php';
-	}
-
-	public function suggestions(){
-		echo "suggestions";
 	}
 
 	public function desinscription(){
@@ -24,6 +12,18 @@ class Coursescontroller extends Controller{
                 
         $MODELcours->remove($student, $cours[intval($_GET["cours"])-1]);
         header('location: '.URL.'Student');
+	}
+
+	public function Exercice(){
+		$MODELcours = $this->loadModel('CourseSubstcription');
+		
+		$student=&PersonFactory::getPerson($_SESSION["email"]);
+		$cours = $MODELcours->getCourses($student);	
+
+		//print_r($_GET);
+		require 'application/views/_templates/header.php';
+        require 'application/views/student_exercice.php';
+        require 'application/views/_templates/footer.php';		
 	}
 }
 ?>
