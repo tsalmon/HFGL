b5bc9cc8a1de538d75cf12c1675db439e439e0a8
tsalmon <th_s@hotmail.fr>
2014-04-22 17:15:20 +0200

    desinscription course

--- src/application/controller/Coursescontroller.php
+++ src/application/controller/Coursescontroller.php
@@ -2,6 +2,17 @@
 class Coursescontroller extends Controller{
 	public function index(){
 		print_r($_GET);
-	}		
+	}
+
+	public function desinscription(){
+		echo "<p>".print_r($_GET)."</p>";
+		$MODELcours = $this->loadModel('CourseSubstcription');
+		//$MODELstudent = $this->loadModel('PersonFactory');
+        
+		$student=&PersonFactory::getPerson($_SESSION["email"]);
+		$cours = $MODELcours->getCourses($student);
+                
+        $MODELcours->remove($student, $cours[intval($_GET["cours"])-1]);
+	}
 }
 ?>
