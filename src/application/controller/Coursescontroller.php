38a5e5d21756c42411bc7f0e78d10bbd93a0bbfa
tsalmon <th_s@hotmail.fr>
2014-04-23 17:17:02 +0200

    can get the chapter of the part of the course selected

--- src/application/controller/Coursescontroller.php
+++ src/application/controller/Coursescontroller.php
@@ -2,28 +2,24 @@
 class Coursescontroller extends Controller{
 	public function index(){
 		$MODELcours = $this->loadModel('CourseSubstcription');
-		
+
 		$student=&PersonFactory::getPerson($_SESSION["email"]);
 		$cours = $MODELcours->getCourses($student);	
+		$cours = $cours[intval($_GET["cours"])-1]->parts()[intval($_GET["part"])-1]->chapters()[intval($_GET["cours"])-1];
 
-		//print_r($_GET);
 		require 'application/views/_templates/header.php';
-        require 'application/views/student_exercice.php';
+        require 'application/views/student_viewChapter.php';
         require 'application/views/_templates/footer.php';		
 	}
 
 	public function desinscription(){
 		$MODELcours = $this->loadModel('CourseSubstcription');
-        
+
 		$student=&PersonFactory::getPerson($_SESSION["email"]);
 		$cours = $MODELcours->getCourses($student);
-                
+
         $MODELcours->remove($student, $cours[intval($_GET["cours"])-1]);
         header('location: '.URL.'Student');
-	}
-
-	public function Chapitre(){
-		
 	}
 }
 ?>
