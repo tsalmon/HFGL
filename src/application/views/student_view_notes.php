7d517f8e122e52d6dc758f21c263a8613e5b4bb6
tsalmon <th_s@hotmail.fr>
2014-04-24 23:30:14 +0200

    prof: listes cours teaching

--- src/application/views/student_view_notes.php
+++ src/application/views/student_view_notes.php
@@ -9,9 +9,7 @@
         <h2>Les notes de tous les cours suivis</h2>
         <table style="width:100%; border-spacing:0;">
           <tr><th>Nom du cours</th> <th>Exercices</th><th>Projet</th><th>Examen</th><th>Note finale</th></tr>
-          <tr><td><a href="#">Cours 1</a></td> <td></td><td></td><td></td><td></td></tr>
-          <tr><td><a href="#">Cours 2</a></td> <td></td><td></td><td></td><td></td></tr>
-          <tr><td><a href="#">Cours 3</a></td> <td></td><td></td><td></td><td></td></tr>
+          
           <tr><td><a href="#">Cours 4</a></td> <td></td><td></td><td></td><td></td></tr>
         </table>
           
