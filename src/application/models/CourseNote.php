6734e0f9dc07ba8f9e30c24210b23b54066e615c
Владислав Фиц <coolermaster@MacBook-Pro-Vladislav.local>
2014-04-25 14:07:17 +0400

    CourseNote completé

--- src/application/models/CourseNote.php
+++ src/application/models/CourseNote.php
@@ -3,10 +3,36 @@ require_once("application/models/Document.php");
 
 class CourseNote extends Document{
     private $URL;
+    private $title;
+    private $description;
 
+    public function setURL($url){
+    	$this->URL = $url;
+    }
 
     public function getURL()
     {
         return $this->URL;
     }
+
+    public function setTitle($title){
+    	$this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setDescription($description){
+    	$this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+
 } 
