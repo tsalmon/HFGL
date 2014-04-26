9a96dc687c36be6c24fcf139ac9b1fe83964e0f1
tsalmon <th_s@hotmail.fr>
2014-04-26 20:57:23 +0200

    createChapter

--- src/application/views/teacher_creerChapitre.php
+++ src/application/views/teacher_creerChapitre.php
@@ -4,14 +4,30 @@
       <div class="content">
         <h3>Création de chapitre</h3>
         <p>Pour le cours "<?php echo $cours->title(); ?>" - partie "<?php echo $part->title();  ?>"</p>
-        <form method="post" action="<?php echo URL.'Professor/CreateChapter_ok?cours='.$_GET["cours"].'&part='.$_GET["part"]; ?>" enctype="multipart/form-data">
+        <form name="chpform" method="post" action="<?php echo URL.'Professor/CreateChapter_ok?cours='.$_GET["cours"].'&part='.$_GET["part"]; ?>" enctype="multipart/form-data">
+          <p>
           <input type="text" name="chp_name" placeholder="nom du chapitre" required/>
           <div>
             <textarea name="chp_descr" placeholder="description"></textarea>
           </div>
           <input type="hidden" name="MAX_FILE_SIZE" value="10000000"/>
           <label for="upload">Leçon :</label> <input type="file" id="upload" name="chp_file_lesson"/>
+        </p>
           <p>
+          <label>A faire a partir du : </label>
+            <select name="avalable_year" onchange="ava_year();"></select>
+           <select name="avalable_month" onchange="ava_month();"></select>    
+           <select name="avalable_day"></select>          
+          </p>
+
+          <p>
+          <label>Jusq'au : </label>
+            <select name="deadline_year" onchange="dead_year();"></select>
+           <select name="deadline_month" onchange="dead_month();"></select>    
+           <select name="deadline_day"></select>          
+          </p>
+
+           <p>
             <input type="submit"/>
           </p>
         </form>
@@ -19,3 +35,5 @@
     </div>
     <?php include("_templates/nav_footer_enseignant.php"); ?>
 </div>
+
+<script src="<?php echo URL.'public/js/ChapterForm.js' ?>"></script>
