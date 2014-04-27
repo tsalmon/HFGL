7fafc5cac791bcb9ed2484638ff695a524caf165
tsalmon <th_s@hotmail.fr>
2014-04-27 18:52:23 +0200

    CourseController, solve bug Professor::studentID

--- src/application/controller/Coursescontroller.php
+++ src/application/controller/Coursescontroller.php
@@ -3,12 +3,13 @@ class Coursescontroller extends Controller{
 	public function index(){
 		$MODELcours = $this->loadModel('CourseSubstcription');
 
-		$student=&PersonFactory::getPerson($_SESSION["email"]);
-		$cours = $MODELcours->getCourses($student);	
-		$cours = $cours[intval($_GET["cours"])-1];
-		$part = $cours->parts()[intval($_GET["part"])-1];
-		$chp = $part->chapters()[intval($_GET["cours"])-1];		
+		$user=&PersonFactory::getPerson($_SESSION["email"]);
 
+		$cours = CourseFactory::getCourse($_GET["cours"], true);
+		$part = $cours->part($_GET["part"]);
+		$chp = $part->chapter($_GET["chp"]);
+
+	
 		require 'application/views/_templates/header.php';
         require 'application/views/student_viewChapter.php';
         require 'application/views/_templates/footer.php';		
