922cde47723fd5436769b0c5905052363531f7f5
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2014-04-29 01:25:57 +0200

    Resource model ajouté, PQuestion rempli

--- src/application/models/PQuestion.php
+++ src/application/models/PQuestion.php
@@ -1,14 +1,41 @@
 <?php
 require_once("application/models/Question.php");
 require_once("application/models/QuestionTypeManager.php");
+require_once("application/models/Resource.php");
 
 class PQuestion extends Question{
-    public function writeToDB(){
+	private $resources;
 
+	public function getResources(){
+		return $this->resources;
+	}
+
+    public function writeToDB(){
+   		echo "INSERT INTO `Question`(`assignment`, `points`, `typeID`) VALUES ('".$this->assignment."',".$this->points.",".QCM.")<br>";
+        PDOHelper::getInstance()->exec("INSERT INTO `Question`(`assignment`, `points`, `typeID`) VALUES ('".$this->assignment."',".$this->points.",".QuestionTypeManager::getInstance()->getQcmID().")");
+        $this->questionID = PDOHelper::getInstance()->lastInsertID();
+        echo "Inserted questionID:".$this->questionID."<br>";
+
+        foreach ($this->resources as $resource)
+        {
+            $resource->writeToDBForQuestionID($this->questionID);
+        }
+
+        return $this->questionID;
     }
 
     public function loadByID($questionID){
+    	$this->questionID = $questionID;
 
+        //Getting resources for current question
+        if($resourcesRequestResult = PDOHelper::getInstance()->query("SELECT * FROM Resource WHERE questionID=".$questionID))
+        {
+            //enumeration of resources
+            while($currentResourceRow = $resourcesRequestResult->fetch(PDO::FETCH_ASSOC))
+            {
+                $this->resources[] = new Resource($currentResourceRow['type'], $currentAnswerRow['content']);
+            }
+        }
     }
 
 } 
---
+++ src/application/models/Resource.php
@@ -0,0 +1,35 @@
+<?php
+
+class Resource {
+    private $questionID;
+    private $type;
+    private $content
+
+    public function getQuestionID(){
+        return $this->questionID;
+    }
+
+    public function getType(){
+        return $this->type;
+    }
+
+    public function getContent(){
+        return $this->content;
+    }
+
+    public function __construct($type,$content)
+    {
+        $this->content = $content;
+        $this->type = $type;
+    }
+
+    function __destruct() {
+        unset($this->content);
+    }
+
+    public function writeToDBForQuestionID($questionID){
+        echo "INSERT INTO `Resource`(`questionID`, `content`, `type`) (".$questionID.",'".$this->content."',".($this->type).")<br>";
+        PDOHelper::getInstance()->exec("INSERT INTO `Responses`(`questionID`, `content`, `type`) VALUES (".$questionID.",'".$this->content."',".($this->type).")");
+        return PDOHelper::getInstance()->lastInsertID();
+    }
+} 
