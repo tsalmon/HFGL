ddebe4d9d829f247af900026cb69886cd1f95b34
tsalmon <th_s@hotmail.fr>
2014-04-28 19:40:05 +0200

    JS: testQCM: check if a question is a QCM, and then if there is duplicates values and/or no checked box

--- src/public/js/exercice.js
+++ src/public/js/exercice.js
@@ -1,3 +1,34 @@
+function testQCM(){
+	if(document.forms.qtform.lareponse.value != "checkbox"){
+		return true;
+	}
+	var nb_checkbox = document.getElementById("reponsenb").value;
+	var no_check = true;
+	var responses = [];
+	for(i = 0; i < nb_checkbox; i++){
+		if(document.getElementById('c'+i).checked){
+            no_check = false;
+        }
+        responses.push(document.getElementById('r'+i).value);
+	}	
+	if(no_check){
+		alert("Vous devez cocher au moins une case");
+		return false;
+	}
+	return uniqueResponse(responses);
+}
+
+function uniqueResponse(responses){
+	responses.sort();
+	for (var i = 0; i < arr.length - 1; i++) {
+    	if (sorted_arr[i + 1] == sorted_arr[i]) {
+    		alert("Il ne doit pas y avoir de doublons dans les réponses d'un QCM");
+    		return false;
+    	}
+	}
+	return true;
+}
+
 function url(selectUrl) {
 	url=selectUrl.options[selectUrl.SelectedIndex].value ;
 	getElementById( 'eval ').action=url ;
@@ -5,15 +36,10 @@ function url(selectUrl) {
 }
 
 function rep(){
-	/*if(document.getElementById('lareponse').value=="text"){
-		document.getElementById('thereponse').innerHTML='<input type= "text " name= "reponse "/ placeholder="ecrivez ici la réponse" required>';
-		document.getElementById('reponses').value="1";
-	}*/
-	if(document.getElementById('lareponse').value=="textarea"){
+	if(document.getElementById('lareponse').value=="libre"){
 		document.getElementById('laquestion').innerHTML='<input type="text" name="question" required/>';
 
 		document.getElementById('thereponse').innerHTML='<textarea name="reponse" disabled></textarea>';
-		document.getElementById('reponsetype').value="2";	
 	}
 	if(document.getElementById('lareponse').value=="checkbox"){
 		document.getElementById('laquestion').innerHTML='<input type="text" name="question" required/>';
@@ -44,12 +70,12 @@ function combien(string){
 	for(i=0;i<how;i++){
 		// si string = 3, c'est un checkbox , sinon c'est un radio
 		if(string=="3"){
-			document.getElementById('thereponse').innerHTML=document.getElementById('thereponse').innerHTML+'<br /><input type= "text" placeholder="ecrivez la réponse possible" name= "r[]" required/><input type="checkbox"  name= "c'+i+'" />';
+			document.getElementById('thereponse').innerHTML=document.getElementById('thereponse').innerHTML+'<br /><input type= "text" placeholder="ecrivez la réponse possible" id="r'+i+'" name= "r[]" required/><input type="checkbox" id=c'+i+' name= "c'+i+'" />';
 		}
-		else{
-			document.getElementById('thereponse').innerHTML=document.getElementById('thereponse').innerHTML+'<br /><input type= "text" placeholder="ecrivez la bonne réponse" name="r[]" size="20" required/>';
+		else{	
+			document.getElementById('thereponse').innerHTML=document.getElementById('thereponse').innerHTML+'<br /><input type= "text" placeholder="ecrivez la bonne réponse" id="r'+i+'" name="r[]" size="20" required/>';
 		}
 	}
-	document.getElementById('reponsetype').value=string;
+	document.getElementById('reponsenb').value=how;
 }	
 
