01003c28cdce4e64febb32cfada61176524ec6f7
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2014-05-12 10:16:25 +0200

    css for inscription un cours

--- src/application/views/student_inscrireCours.php
+++ src/application/views/student_inscrireCours.php
@@ -5,15 +5,12 @@
     <div id="site_content">
 
       <div id="welcome"><h3>Bienvenue</h3></div>
-
-<<<<<<< HEAD
-      <div class="content_big">
         
+     
+
+      <div class="content_big">
         <?php print_r($liste_cours); ?>
 
-=======
-      <div class="content">
->>>>>>> 5a4ef011e4d17583baa75e19b7c595f72dfdca3a
         <h2>Les suggestions de cours</h2>
         <?php
         foreach ($suggestions as $key => $value) {
@@ -31,6 +28,7 @@
         ?>          
       </div>
     </div>
+
     
     <?php include("_templates/nav_footer_etudiant.php"); ?>
 </div>
