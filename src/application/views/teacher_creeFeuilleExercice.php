0a9c032adf3daab003e41e83d6c54f9e17a4eaf9
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2014-05-19 00:34:55 +0200

    Feuille exercice

--- src/application/views/teacher_creeFeuilleExercice.php
+++ src/application/views/teacher_creeFeuilleExercice.php
@@ -5,84 +5,10 @@
 
       <?php include("_templates/bienvenue_title.php"); ?>
       
-      <div class="content_big">
-        <h3>Création d'exercice</h3>
-        <?php
-          $qt_nb = (!isset($_POST["nb_qt"])) ? 1 : $_POST["nb_qt"] + 1;
-        ?>
-        <p>Question n°<?php echo $qt_nb; ?></p>
-        <?php
-//          Controller::print_dbg($_POST);
-        ?>
-        <form name="qtform" method="POST" action="<?php echo URL.'Professor/CreateExercice'?>" onsubmit="return testQCM();">
-          <input type="hidden" name="nb_qt" value="<?php echo $qt_nb; ?>"/>
-          <table>
-            <tr>
-              <td>
-                Question
-              </td>
-              <td>
-                <span id="laquestion"><input type="text" name="question" required/></span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Conseil
-              </td>
-              <td>
-                <input type="text" name="tip" required/></span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Points
-              </td>
-              <td>
-                <input type="number" name="points" required/></span>
-              </td>
-            </tr>            
-            <tr>
-              <td>
-                Reponse
-                <select name="lareponse" id="lareponse" onchange="rep()">
-                  <option value="libre">
-                    Réponse libre
-                  </option>
-                  <option value="checkbox">
-                    QCM
-                  </option>
-                  <option value="lines">
-                    QRF
-                  </option>
-                 <option value="code">
-                    Programme
-                  </option>
-                </select>
-                <input type="hidden" id="reponsenb" name="reponsenb" value="libre" />
-              </td>
-              <td name="thereponse" id="thereponse">
-                <textarea disabled></textarea>
-                <!--<input type="text" name="reponse" placeholder="ecrivez ici la réponse" required/>-->
-              </td>
-            </tr>      
-            <tr id="student_corrector">
-                <td>Correction par pairs :</td>
-                <td><input type="checkbox" id="student_corrector_box" name="student_corrector" value="student_corrector" /></td>
-            </tr>  
-          </table>  
-          <ul>
-            <li><input type="submit" value="Valider et finir"/></li>
-            <li><input type="submit" value="Valider et continuer"/></li>
-            <?php
-              if(false){ // todo
-                echo '<li><input type="submit" value="Annuler la question et finir"/></li>';
-              }
-            ?>
-          </ul>
       <div id="createExercice" class="content_big">
         <form action="CreateExerciceFork" method="post" enctype="multipart/form-data">
           <fieldset>
-            <legend><h2>Créer le nouveau questionnaire</h2></legend>
+            <legend><h2>Créer la nouvelle questionnaire</h2></legend>
 
             <div class="form_settings">
               <p><span>Description:</span>           
@@ -118,4 +44,3 @@
 
     <?php include("_templates/nav_footer_enseignant.php"); ?>
 </div>
-
