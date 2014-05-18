1120e116fb6b199b58fc1d2d940d961a56a8630f
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2014-05-18 16:07:00 +0200

    BDD

--- src/application/_install/database.sql
+++ src/application/_install/database.sql
@@ -230,11 +230,11 @@ PRIMARY KEY (`roleID`)
 );
 
 CREATE TABLE `StudentEstimation` (
-`estimatingStudentID` int NOT NULL,
+`estimatingPersonID` int NOT NULL,
 `estimatedStudentID` int NOT NULL,
 `questionID` int NOT NULL,
 `delegatedTutorID` int NOT NULL,
-PRIMARY KEY (`estimatingStudentID`, `questionID`, `estimatedStudentID`, `delegatedTutorID`) 
+PRIMARY KEY (`estimatingPersonID`, `questionID`, `estimatedStudentID`, `delegatedTutorID`) 
 );
 
 
@@ -267,7 +267,7 @@ ALTER TABLE `Test` ADD CONSTRAINT `fk_Test_Question_1` FOREIGN KEY (`questionID`
 ALTER TABLE `Resource` ADD CONSTRAINT `fk_Resource_Question_1` FOREIGN KEY (`questionID`) REFERENCES `Question` (`questionID`);
 ALTER TABLE `Part` ADD CONSTRAINT `fk_Part_Questionnaire_1` FOREIGN KEY (`questionnaireID`) REFERENCES `Questionnaire` (`questionnaireID`);
 ALTER TABLE `Person` ADD CONSTRAINT `fk_Person_Role_1` FOREIGN KEY (`roleID`) REFERENCES `Role` (`roleID`);
-ALTER TABLE `StudentEstimation` ADD CONSTRAINT `fk_StudentGrading` FOREIGN KEY (`estimatingStudentID`) REFERENCES `Student` (`studentID`);
+ALTER TABLE `StudentEstimation` ADD CONSTRAINT `fk_StudentGrading` FOREIGN KEY (`estimatingPersonID`) REFERENCES `Person` (`personID`);
 ALTER TABLE `StudentEstimation` ADD CONSTRAINT `fk_StudentGrading_1` FOREIGN KEY (`estimatedStudentID`) REFERENCES `Student` (`studentID`);
 ALTER TABLE `StudentEstimation` ADD CONSTRAINT `fk_StudentGrading_2` FOREIGN KEY (`questionID`) REFERENCES `Question` (`questionID`);
 ALTER TABLE `StudentEstimation` ADD CONSTRAINT `fk_StudentEstimation` FOREIGN KEY (`delegatedTutorID`) REFERENCES `Tutor` (`tutorID`);
