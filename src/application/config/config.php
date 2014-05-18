1e8cd5e6af8c01e2473c6016f91618e8621a69c8
tsalmon <th_s@hotmail.fr>
2014-05-18 15:11:20 +0200

    config

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -6,8 +6,6 @@ ini_set("display_errors", 1);
  /* Configuration for: Project URL*/
  define('URL', 'http://localhost/GL/src/');
 
-
-
 /*Configuration for: Database*/
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
